70061b8ce89a31d2265960bdd9515cb52d43ec9f
Habeeb Odunsi <habeebodunsi@yahoo.com>
2020-04-10 20:18:02 +0100

    test frontend deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Advisor
+          Welcome to Advisor ui
         </p>
         <a
           className="App-link"

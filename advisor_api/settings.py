2409cacf2ee1b8b4aa33090ee38967efb08a784b
Habeeb <habeebodunsi@yahoo.com>
2020-04-11 19:52:11 +0100

    Feature/GitHub actions (#12)
    * add requirements file

--- advisor_api/settings.py
+++ advisor_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_c6@oi7yioddo-0mz!=yenzso0!@8-q%qa1-x*5sn^*gx^@m8i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['advisor-api.herokuapp.com']
 
 
 # Application definition
@@ -122,4 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)

fcbc67cae3cc5e399dd3bfc0f701142a42fcfe7a
Lee <114220313@umail.ucc.ie>
2020-05-06 21:29:23 +0100

    add function to store okta_id

--- user/serializers.py
+++ user/serializers.py
@@ -8,8 +8,8 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ('id', 'email', 'password', 'name')
-        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+        fields = ('id', 'email', 'name', 'okta_id')
+        extra_kwargs = {'okta_id': {'read_only': True}}
 
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
--- user/views.py
+++ user/views.py
@@ -1,10 +1,11 @@
 from rest_framework import generics
 from user.serializers import UserSerializer
-from okta_jwt.jwt import validate_token
+from okta_jwt.jwt import validate_token, generate_token
 from django.http import JsonResponse
 from okta.models.user import User
 from okta import UsersClient
 from core.models import Config
+from django.contrib.auth import get_user_model
 
 config = Config()
 
@@ -14,7 +15,8 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
     def post(self, request, *args, **kwargs):
-
+        # access_token = generate_token(config.issuer, config.client_id, config.client_secret, "leebusiness197@gmail.com","Advisor2020")
+        # print(access_token)
         access_token = request.META.get('HTTP_AUTHORIZATION')
         try:
             validate_token(access_token, config.issuer, config.aud, config.client_id)
@@ -32,7 +34,12 @@ class CreateUserView(generics.CreateAPIView):
             try:
                 user = users_client.create_user(new_user, activate=False)
                 if user.id is not None:
-                    return super().post(request)
+                    advisor_user = get_user_model().objects.create_user(
+                        email=serializer.data['email'],
+                        name=serializer.data['name'],
+                        okta_id=user.id
+                    )
+                    return JsonResponse({"result": {"user_id": advisor_user.id}}, status=200)
 
             except Exception as e:
                 return e.args[0]

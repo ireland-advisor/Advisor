60dbaf92aa2a1f861c18f6aadcfc33ceef1be556
Lee <114220313@umail.ucc.ie>
2020-05-17 13:13:24 +0100

    add cors

--- advisor_api/settings.py
+++ advisor_api/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'drf_yasg',
     'user',
     'core',
@@ -76,6 +77,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'advisor_api.urls'
